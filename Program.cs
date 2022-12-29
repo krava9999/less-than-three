2351e7fd9fa55778f1a808ae32d3e2d1945537eb
krava9999 <kosmo@internet.ru>
2022-12-29 22:29:34 +1000

    Новый метод, подсичтывает количество элементов основного массива, длина которыйх меньше или равна трем

--- Program.cs
+++ Program.cs
@@ -1,22 +1,35 @@
 ﻿string[] getArrWereLenghLessThree(string[] arr)
 {
 
-    string[] boxArr = new string[arr.Length];
-
+    int count = getCountElemntMoreThree(arr);
+    string[] boxArr = new string[count];
+    int indexBox = 0;
     for (int i = 0; i < arr.Length; i++)
     {
         if (arr[i].Length <= 3)
         {
-            boxArr[i] = arr[i];
+            boxArr[indexBox] = arr[i];
+            indexBox++;
         }
 
     }
     return boxArr;
 }
 
+int getCountElemntMoreThree(string[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i].Length <= 3)
+        {
+            count++;
+        }
+    }
+    return count;
+}
 
 string[] arr = { "Sunday", "Monday", "Tue", "Wed", "", "Friday", "Sa" };
 arr = getArrWereLenghLessThree(arr);
-
-
 Console.Write($"Ваш массив: [{string.Join(", ", arr)}] \n");
+
