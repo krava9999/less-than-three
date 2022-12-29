578844a5147761e808b6bc1eff557fdfa4034031
krava9999 <kosmo@internet.ru>
2022-12-29 22:28:07 +1000

    Новый метод, создающий новый массив строк в который записываются элементы длина символов который меньше или равна 3

--- Program.cs
+++ Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] getArrWereLenghLessThree(string[] arr)
+{
+
+    string[] boxArr = new string[arr.Length];
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i].Length <= 3)
+        {
+            boxArr[i] = arr[i];
+        }
+
+    }
+    return boxArr;
+}
+
+
+string[] arr = { "Sunday", "Monday", "Tue", "Wed", "", "Friday", "Sa" };
+arr = getArrWereLenghLessThree(arr);
+
+
+Console.Write($"Ваш массив: [{string.Join(", ", arr)}] \n");
